75f76666f85cf1dd400681b718a7d1c50fc22130
aziddownload <59896910+aziddownload@users.noreply.github.com>
2023-10-28 05:01:32 +0700

    Delete obfuscator directory

--- obfuscator/App/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import {combineReducers} from 'redux'
-
-import {code} from './code';
-import {options} from './options';
-
-export default combineReducers({
-    code,
-    options,
-});
--- obfuscator/App/util/get-options-markdown.js
+++
@@ -1,7 +0,0 @@
-const fullReadmeMarkdown = require('javascript-obfuscator/README.md').default;
-
-export function getOptionsMarkdown() {
-    return fullReadmeMarkdown
-        .split('<!-- ##options-start## -->')[1]
-        .split('<!-- ##options-end## -->\n')[0];
-}
